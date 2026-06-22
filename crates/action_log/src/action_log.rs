2ee2153ca04cd27f477ca8863057174a378b468e
Nick Ebert <nick.ebert2@gmail.com>
2026-06-22 01:10:54 -0700

    action_log: adapt restore_edit to new BufferDiff::new API

--- crates/action_log/src/action_log.rs
+++ crates/action_log/src/action_log.rs
@@ -1109,11 +1109,7 @@ impl ActionLog {
         let text_snapshot = buffer.read(cx).text_snapshot();
         let language = buffer.read(cx).language().cloned();
         let language_registry = buffer.read(cx).language_registry();
-        let diff = cx.new(|cx| {
-            let mut diff = BufferDiff::new(&text_snapshot, cx);
-            diff.language_changed(language, language_registry, cx);
-            diff
-        });
+        let diff = cx.new(|cx| BufferDiff::new(&text_snapshot, language, language_registry, cx));
         let (diff_update_tx, diff_update_rx) = mpsc::unbounded();
         let tracked_buffer = TrackedBuffer {
             buffer: buffer.clone(),
